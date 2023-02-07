3f27f9748edfccb4b936873095b25e721ac01fb3
KaveCoding <teachereli@outlook.com>
2023-02-07 11:38:30 +0100

    Första commit

---
+++ Väderkollen/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+
+namespace Väderkollen
+{
+    internal class Program
+    {
+        public static string path = "../../../Files/";
+        static void Main(string[] args)
+        {
+
+          
+        /*Utomhus
+        Medeltemperatur och luftfuktighet per dag, valt datum, sökmöjlighet
+        Sortering av varmast till kallaste dagen enligt medeltemperatur per dag
+        sortering av torrast till fuktigaste dagen enligt medelluftfuktighet per dag
+        Sortering av minst till störst risk av mögel
+        Datum för meteorologisk Höst
+        Datum för meteologisk vinter
+        */
+
+        /*Inomhus
+        Medeltemperatur per dag, valt datum
+        Sortering av varmast till kallaste dagen enligt medeltemperatur per dag
+        sortering av torrast till fuktigaste dagen enligt medelluftfuktighet per dag
+        Sortering av minst till störst risk av mögel
+        */
+
+
+        static void ReadLines(string filename)
+            {
+                using (StreamReader reader = new StreamReader(path + filename))
+                {
+
+
+                    int lineNumber = 0;
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        lineNumber++;
+                        Console.WriteLine("Rad " + lineNumber + " " + line);
+                        line = reader.ReadLine();
+                    }
+
+
+                }
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
