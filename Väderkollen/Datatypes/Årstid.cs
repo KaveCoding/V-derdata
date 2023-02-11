d24eb74f57c533614e3096c6fbbaac395ba1e9e9
Ondastsaft <kenneth.jansson@gmail.com>
2023-02-11 20:48:00 +0100

    Revert "Start mögelalgoritm"
    This reverts commit 52e71c7fa49e208eee7df2587e7af1e5df2c6704.

--- Väderkollen/Datatypes/Årstid.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Väderkollen.Datatypes
-{
-    internal class Årstid
-    {
-        public int Dag { get; set; }
-        public int Månad { get; set; }
-
-    }
-}
---
+++ Väderkollen/Årstid.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Väderkollen
+{
+    internal class Årstid
+    {
+        public int Dag { get; set; }
+        public int Månad { get; set; }
+
+    }
+}
